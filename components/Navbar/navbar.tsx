a0e89e66e25ee08bebfbab1819880317799264c6
KACPER SINILO <kacperosinilo@gmail.com>
2023-12-30 12:59:13 +0100

    fix(navbar): transform to use client

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,27 +1,30 @@
-import dynamic from "next/dynamic"
+"use client"
+
+import dynamicImport from "next/dynamic"
 import Link from "next/link"
 import { Navigation } from "@/enums/navigation"
-import { getServerSession, User } from "next-auth"
+import { User } from "next-auth"
+import { useSession } from "next-auth/react"
 
 import { buttonVariants } from "@/components/ui/button"
 import Container from "@/components/ui/container"
 import Logo from "@/components/ui/logo"
 
-const UserButton = dynamic(() => import("./user-button"), {
+const UserButton = dynamicImport(() => import("./user-button"), {
     loading: () => null,
     ssr: true,
 })
 
-async function Navbar() {
-    const session = await getServerSession()
+function Navbar() {
+    const session = useSession()
 
     return (
         <div className="mb-10 shadow-sm">
             <Container>
                 <nav className="flex h-20 items-center justify-between py-5">
                     <Logo />
-                    {session ? (
-                        <UserButton user={session.user as User} />
+                    {session.data?.user ? (
+                        <UserButton user={session.data.user as User} />
                     ) : (
                         <Link
                             className={buttonVariants({ variant: "default" })}
